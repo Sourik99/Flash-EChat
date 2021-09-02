e788702b070348f4b2c4534c76758f564fdbc6dd
Sourik Basu <89473107+Sourik99@users.noreply.github.com>
2021-09-02 09:31:38 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,22 +1,24 @@
-const io = require('socket.io')(3000, {
-    cors: {
-      origin: '*',
-    }
-  });
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const users = {};
 
-io.on('connection', (socket) =>{
-    socket.on('new-user-joined', names =>{
-        // console.log("New user",names)
-        users[socket.id] = names;
-        socket.broadcast.emit('user-joined', names);
-    })
-    socket.on('send' , message =>{
-        socket.broadcast.emit('receive', {message: message , names: users[socket.id]})
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (names) => {
+    users[socket.id] = names;
+    socket.broadcast.emit("user-joined", names);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      names: users[socket.id],
     });
-    socket.on('disconnect' , message =>{
-        socket.broadcast.emit('leave' , users[socket.id])
-        delete users[socket.id];
-    });
-})
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("leave", users[socket.id]);
+    delete users[socket.id];
+  });
+});
